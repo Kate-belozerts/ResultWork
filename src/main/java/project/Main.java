5eb6f8cd26b7e0ee320917f3400370c8d33fe859
Kate Belozertseva <katebelozertseva@gmail.com>
2023-07-10 16:48:35 +0300

    Start programm

---
+++ src/main/java/project/Main.java
@@ -0,0 +1,10 @@
+package project;
+
+import project.terminal.Input;
+
+public class Main {
+    public static void main(String[] args) {
+        Input input = new Input();
+        input.start();
+    }
+}
